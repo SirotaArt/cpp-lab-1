6f96b2c4ba29a325e6ebc5d707001637075d840b
SirotaArt <super-geschaft@yandex.ru>
2023-10-02 21:26:45 +0300

    test

--- lab-2.cpp
+++ lab-2.cpp
@@ -3,21 +3,29 @@
 using namespace std;
 double t(double x); double fact(double x); double div(double y);
 
-double t(double x) {
-	int k1, k2;
-	double a1, a2;
-	a1 = 0; a2 = 0;
-	k1 = k2 = 0;
+
+
+
+double t(double x) 
+{
+	int k1, k2; double a1, a2;
+	a1 = a2 = k1 = k2 = 0;
 	while (k1 != 11) {
-		a1 += pow(x, 2 * k1 + 1) / fact(2 * k1 + 1); k1 += 1;
+		a1 += pow(x, 2 * k1 + 1) / fact(2 * k1 + 1); 
+		k1 += 1;
 	}
 	while (k2 != 11) {
-		a2 += pow(x, 2 * k2) / fact(2 * k2); k2 += 1;
+		a2 += pow(x, 2 * k2) / fact(2 * k2); 
+		k2 += 1;
 	}
 	return (a1 / a2);
 }
 
-double fact(double x) {
+
+
+
+double fact(double x) 
+{
 	if (x <= 0) {
 		return 1;
 	}
@@ -27,9 +35,12 @@ double fact(double x) {
 }
 
 
-double div(double y) {
+
+double div(double y) 
+{
 	return  (7 * t(0.25) + 2 * t(1 + y)) / (6 - t(y * y - 1));
 }
+
 
 
 int main()
@@ -37,5 +48,4 @@ int main()
 	double y;
 	cin >> y;
 	cout << div(y);
-
 }
