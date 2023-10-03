64bb7d581bdc8435c08e2e363e698f8906cfe18e
SirotaArt <Super-Geschaft@yandex.ru>
2023-10-03 11:10:29 +0300

    lab - 2

--- lab-2.cpp
+++ lab-2.cpp
@@ -1,28 +1,22 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-double t(double x); double fact(double x); double div(double y);
-
-
 
 
 double t(double x) 
 {
-	int k1, k2; double a1, a2;
-	a1 = a2 = k1 = k2 = 0;
+	int k1, k2;
+	double a1, a2;
+	a1 = 0; a2 = 0;
+	k1 = k2 = 0;
 	while (k1 != 11) {
-		a1 += pow(x, 2 * k1 + 1) / fact(2 * k1 + 1); 
-		k1 += 1;
+		a1 += pow(x, 2 * k1 + 1) / fact(2 * k1 + 1); k1 += 1;
 	}
 	while (k2 != 11) {
-		a2 += pow(x, 2 * k2) / fact(2 * k2); 
-		k2 += 1;
+		a2 += pow(x, 2 * k2) / fact(2 * k2); k2 += 1;
 	}
 	return (a1 / a2);
 }
-
-
-
 
 double fact(double x) 
 {
@@ -35,12 +29,10 @@ double fact(double x)
 }
 
 
-
 double div(double y) 
 {
 	return  (7 * t(0.25) + 2 * t(1 + y)) / (6 - t(y * y - 1));
 }
-
 
 
 int main()
@@ -48,4 +40,5 @@ int main()
 	double y;
 	cin >> y;
 	cout << div(y);
+
 }
