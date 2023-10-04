e6b4a288b44870735555737ee1c3b7193a70f426
SirotaArt <Super-Geschaft@yandex.ru>
2023-10-04 15:45:09 +0300

    lab-2

--- lab-3.cpp
+++ lab-3.cpp
@@ -1,29 +1,42 @@
 #include <iostream>
 #include <cmath>
-#include <vector>
 using namespace std;
+int sor(int arr[], int n);
 
 int main()
 {
 	int n;
 	cin >> n;
-	vector <int> a(n);
-	for (int i = 0; i < n; i++) {
-		cin >> a[i];
+
+	const int p = 32;
+	int arr[p];
+	for (int i = 0; i < n; i++) 
+	{
+		cin >> arr[i];
 	}
 
-	for (int i = 0; i < n; i++) {
-		int num_min = i;
-		for (int j = i + 1; j < n; j++) { if (a[j] < a[num_min]) {num_min = j;}}
-		int temp;
-		temp = a[i];
-		a[i] = a[num_min];
-		a[num_min] = temp;
+	cout << sor(arr, n);
+}
+
+int sor(int arr[], int n)
+{
+	int prefix_sum[sizeof(arr)]; 
+	int suffix_sum[sizeof(arr)];
+	prefix_sum[0] = arr[0];
+
+	for (int i = 1; i < n; i++) {prefix_sum[i] = prefix_sum[i - 1] + arr[i];}
+
+	suffix_sum[n - 1] = arr[n - 1];
+
+	for (int i = n - 2; i >= 0; i--){suffix_sum[i] = suffix_sum[i + 1] + arr[i];}
+
+	int minDiff = pow(10,8);
+
+	for (int i = 0; i < n - 1; i++)
+	{
+		int diff = abs(prefix_sum[i] - suffix_sum[i + 1]);
+
+		if (diff < minDiff) { minDiff = diff;}
 	}
-
-
-	for (int i = 0; i < n; i++) {
-		cout << a[i] << " ";
-	}
-
+	return minDiff;
 }
