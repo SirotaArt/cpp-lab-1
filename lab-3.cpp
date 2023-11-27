a8b9415492ea72aae9d79e47b4176e7db36c94c9
SirotaArt <super-geschaft@yandex.ru>
2023-11-27 18:40:51 +0300

    new

--- lab-3.cpp
+++ lab-3.cpp
@@ -9,45 +9,62 @@ int main()
 	cin >> n;
 	const int p = 32;
 	int arr[p];
-	for (int i = 0; i < n; i++)
+
+	for (int i = 0; i < n; i++)    // Ввод чисел
 	{
 		cin >> arr[i];
 	}
-	for (int j = 0; j < n; j++) { 
+
+	for (int j = 0; j < n; j++)  //Сортировка
+	{ 
 		int num_min = j;
-		for (int i = j; i < n; i++) { 
-			if (arr[i] < arr[num_min]) {
+
+		for (int i = j; i < n; i++) 
+		{ 
+			if (arr[i] < arr[num_min]) 
+			{
 				num_min = i;
 			}
 		}
+
 		int temp;
 		temp = arr[j];
 		arr[j] = arr[num_min];
 		arr[num_min] = temp;
 	}
+
 	cout << sor(arr, n);
 }
 
 int sor(int arr[], int n)
 {
-	int pref[sizeof(arr)];
+	int pref[sizeof(arr)]; 		//Создание перфикс. и суфикс. массивов
 	int suf[sizeof(arr)];
 	pref[0] = arr[0];
 
-	for (int i = 1; i < n; i++) { pref[i] = pref[i - 1] + arr[i]; }
+	for (int i = 1; i < n; i++) 		//Заполнение преф.
+	{ 
+		pref[i] = pref[i - 1] + arr[i]; 
+	}
 
 	suf[n - 1] = arr[n - 1];
 
-	for (int i = n - 2; i >= 0; i--) { suf[i] = suf[i + 1] + arr[i]; }
+	for (int i = n - 2; i >= 0; i--) 	//Заполнение суф.
+	{ 
+		suf[i] = suf[i + 1] + arr[i]; 
+	}
 
 	int minDiff = pow(10, 8);
 
-	for (int i = 0; i < n - 1; i++)
+	for (int i = 0; i < n - 1; i++) 	//Поиск наименьшей разницы между элементами суф. и преф. массивов
 	{
 		int diff = abs(pref[i] - suf[i+1]);
 
-		if (diff < minDiff) { minDiff = diff; }
+		if (diff < minDiff) 
+		{ 
+			minDiff = diff; 
+		}
 	}
-	return minDiff;
 
+	return minDiff;
 }
